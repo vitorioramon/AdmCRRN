756e5e4c339cc5143e5a963fc17ba7ec2f25035a
Producao01 <Producao01@tec-soft.com.br>
2012-05-13 20:22:17 -0300

    Primeiro publicacao do aplicativo

---
+++ Scripts/customvalidation.js
@@ -0,0 +1,117 @@
+﻿var CustomValidation = {};
+
+CustomValidation.Init = function () {
+    //CustomValidationCPF
+    jQuery.validator.addMethod('customvalidationcpf', function (value, element, params) {
+        if (cpfValido(value, element, params))
+            return true;
+        else if (cnpjValido(value, element))
+            return true;
+
+        return false;
+    });
+    jQuery.validator.unobtrusive.adapters.add('customvalidationcpf', {}, function (options) {
+        options.rules['customvalidationcpf'] = true;
+        options.messages['customvalidationcpf'] = options.message;
+    });
+}
+
+//executa -- importante que isso seja feito antes do document.ready
+CustomValidation.Init();
+
+
+//FUNÇÃO PARA VALIDAR CPF
+function cpfValido (cpf, element, params) {
+
+    cpf = jQuery.trim(cpf); // retira espaços em branco
+
+    cpf = cpf.replace('.', '');
+    cpf = cpf.replace('.', '');
+    cpf = cpf.replace('.', '');
+    cpf = cpf.replace('-', '');
+
+    if (cpf.length != 11)
+        return false;
+
+    if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
+        return false;
+    add = 0;
+    for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(9)))
+        return false;
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(10)))
+        return false;
+
+    return true; //cpf válido
+}
+
+
+//FUNÇÃO PARA VALIDAR CNPJ
+function cnpjValido (cnpj, element) {
+
+    cnpj = jQuery.trim(cnpj); // retira espaços em branco
+    // DEIXA APENAS OS NÚMEROS
+    cnpj = cnpj.replace('/', '');
+    cnpj = cnpj.replace('.', '');
+    cnpj = cnpj.replace('.', '');
+    cnpj = cnpj.replace('-', '');
+ 
+   var numeros, digitos, soma, i, resultado, pos, tamanho, digitos_iguais;
+   digitos_iguais = 1;
+ 
+   if (cnpj.length != 14){
+      return false;
+   }
+   for (i = 0; i < cnpj.length - 1; i++){
+      if (cnpj.charAt(i) != cnpj.charAt(i + 1)){
+         digitos_iguais = 0;
+         break;
+      }
+   }
+ 
+   if (!digitos_iguais){
+      tamanho = cnpj.length - 2
+      numeros = cnpj.substring(0,tamanho);
+      digitos = cnpj.substring(tamanho);
+      soma = 0;
+      pos = tamanho - 7;
+ 
+      for (i = tamanho; i >= 1; i--){
+         soma += numeros.charAt(tamanho - i) * pos--;
+         if (pos < 2){
+            pos = 9;
+         }
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+      if (resultado != digitos.charAt(0)){
+         return false;
+      }
+      tamanho = tamanho + 1;
+      numeros = cnpj.substring(0,tamanho);
+      soma = 0;
+      pos = tamanho - 7;
+      for (i = tamanho; i >= 1; i--){
+         soma += numeros.charAt(tamanho - i) * pos--;
+         if (pos < 2){
+            pos = 9;
+         }
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+      if (resultado != digitos.charAt(1)){
+         return false;
+      }
+      return true;
+   }else{
+      return false;
+   }
+};
