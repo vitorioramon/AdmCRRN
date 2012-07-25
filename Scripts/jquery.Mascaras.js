703dd131fa8478df4ece1dc4749943653312b238
Yan Justino <yanjustino@yahoo.com.br>
2012-07-25 14:21:45 -0300

    Alterando leiaute do login

---
+++ Scripts/jquery.Mascaras.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+
+    $(".telefone input[type='text']").mask("(99)9999-9999");
+    $(".data input[type='text']").mask("99/99/9999");
+
+    // Configuração para campos de Real.
+    $(".moeda-real input[type='text']").maskMoney({ showSymbol: true, symbol: "", decimal: ",", thousands: "", allowZero: true });
+});
